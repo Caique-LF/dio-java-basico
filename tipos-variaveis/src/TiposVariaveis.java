ca57b147522f2450bc2a7d7dc48a4492caa9015e
Caique L. Fernandes <caique.l.f9.clf@gmail.com>
2024-05-10 11:29:53 -0400

    Tipo de Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMin = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        // short numeroCurto2 = numeroNormal;
+
+        // int: Representa números inteiros.
+        // double: Representa números decimais.
+        // boolean: Representa valores booleanos (verdadeiro ou falso).
+        // char: Representa caracteres individuais.
+        // byte, short, long, float: Outros tipos numéricos com diferentes tamanhos e
+        // faixas de valores.
+
+        String name = "Caique";
+        String lastName = "Fernandes";
+
+        String fullName = name.concat(" " + lastName);
+
+        System.out.println(fullName);
+
     }
 }
